dad5b7f782feb1cd52dfca05c4abb378d488fe29
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-11 10:25:07 -0300

    refactor:tornando nome de classe e metodo dinamicos

--- src/app/shared/services/uniq-id.service.spec.ts
+++ src/app/shared/services/uniq-id.service.spec.ts
@@ -5,8 +5,8 @@ A função describe recebe dois parâmetros.
 O primeiro é uma descrição do artefato que queremos testar, 
 o segundo é uma função
 */
-describe("UniqueIdService", () => {
-  it("generateUniqueIdWithPrefix: should generate id when called with prefix", () => {
+describe(UniqueIdService.name, () => {
+  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}: should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix("foobar");
     expect(id).toContain("foobar-");
