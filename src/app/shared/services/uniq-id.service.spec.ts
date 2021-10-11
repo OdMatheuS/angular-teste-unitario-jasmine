8a547e182e3958d9f7e4df2a0870f959f830c915
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-11 19:03:29 -0300

    refactor: utlizando beforeEach p/ cada teste it() ter sua propria instancia de UniqueIdService

--- src/app/shared/services/uniq-id.service.spec.ts
+++ src/app/shared/services/uniq-id.service.spec.ts
@@ -6,18 +6,32 @@ O primeiro é uma descrição do artefato que queremos testar,
 o segundo é uma função
 */
 describe(UniqueIdService.name, () => {
+  /**
+   * @method beforeEach
+   * antes de executar cada teste, o valor da variável service vai ser uma nova instância de UniqueIdService,
+   * assim cada teste it() vai ter sua instância de UniqueidService
+   */
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
   it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}: should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix("foobar");
     expect(id.startsWith("foobar-")).toBeTrue();
   });
 
   it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}: should not generate duplicate Id's when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix("foobar"));
     }
     expect(ids.size).toBe(50);
+  });
+
+  it(`${UniqueIdService.prototype.getNumberOfGeneratedUniqueId.name}: should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix("foobar");
+    service.generateUniqueIdWithPrefix("foobar");
+    expect(service.getNumberOfGeneratedUniqueId()).toBe(2);
   });
 });
