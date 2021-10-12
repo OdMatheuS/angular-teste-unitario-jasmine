3b7d2287c317362684fd4db3567e3e3b19c35898
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-12 11:35:30 -0300

    feat: implementado component de likes + atualizando module import e export

--- src/app/shared/components/like-widget/like-widget.component.ts
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-like-widget',
-  templateUrl: './like-widget.component.html',
-  styleUrls: ['./like-widget.component.scss']
+  selector: "app-like-widget",
+  templateUrl: "./like-widget.component.html",
+  styleUrls: ["./like-widget.component.scss"],
 })
 export class LikeWidgetComponent implements OnInit {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
