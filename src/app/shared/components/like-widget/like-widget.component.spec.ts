9db77ea8652d1ba5c681b5f673cbc862f0f0330c
Matheus Nagot <matheusnagot@hotmail.com>
2021-10-13 17:20:25 -0300

    refactor: utilizando beforeEach p/ reaproveitar instancia de um wrapper de component

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -4,27 +4,27 @@ import { LikeWidgetModule } from "./like-widget.module";
 
 describe(`${LikeWidgetComponent.name}`, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null
+  let instanceWrapper:LikeWidgetComponent = null;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports:[LikeWidgetModule]
     }).compileComponents();
+
     fixture = TestBed.createComponent(LikeWidgetComponent)
+    instanceWrapper = fixture.componentInstance
   });
 
   it("should create component", () => {
-    const instanceWrapper = fixture.componentInstance
 
     expect(instanceWrapper).toBeTruthy();
   });
 
   it("Should auto generate ID when id input empty", () => {
-    const instanceWrapper = fixture.componentInstance
     fixture.detectChanges()
     expect(instanceWrapper.id).toBeTruthy();
   });
 
   it("Should NOT generate ID when id input is present", () => {
-    const instanceWrapper = fixture.componentInstance
     const randomId = 'foo'
     instanceWrapper.id = randomId
     fixture.detectChanges()
